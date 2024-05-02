f0a296e7cc95d72fe9ee80db7d9e033e16cbb44b
Thuy Duong Nguyen <122132917+DuongNT199@users.noreply.github.com>
2024-05-03 01:48:45 +0700

    Update day13_problem_solving.sql

--- day13_problem_solving.sql
+++ day13_problem_solving.sql
@@ -150,16 +150,219 @@ WHERE EXISTS (
 GROUP BY EXTRACT(MONTH FROM curr_month.event_date);
 */
 -- EX 6
+SELECT TO_CHAR (trans_date, 'YYYY-MM') AS month
+, country
+, COUNT(id) AS trans_count
+, SUM
+    (CASE
+        WHEN state='approved' THEN 1
+        ELSE 0
+    END) AS approved_count
+, SUM
+    (CASE
+        WHEN state='approved' THEN amount
+        ELSE 0
+    END) AS approved_total_amount
+, SUM(amount) AS trans_total_amount
+FROM Transactions
+GROUP BY month, country
+
+-- Không nên sử dụng JOIN hay CTEs trong bài 6 do test case có TH country ID null hoặc TH không có transaction nào approved => sinh null bảng join, khó xử lí join
 
 -- EX 7
+WITH FY AS
+( 
+    SELECT product_id
+    , MIN(year) AS first_year
+    FROM Sales
+    GROUP BY product_id
+)
+  
+SELECT FY.product_id
+, FY.first_year
+, SL.quantity
+, SL.price
+FROM Sales AS SL
+INNER JOIN FY 
+    ON FY.product_id = SL.product_id
+    AND FY.first_year = SL.year
+
+-- Cần trả về toàn bộ giá và số lượng của mã product trong first_year
+-- Tách bảng lấy key product và year, inner join để lấy toàn bộ record
 
 -- EX 8
+SELECT customer_id
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) >= (SELECT COUNT(product_key) FROM Product)
 
 -- EX 9
+WITH have_mng AS
+(
+    SELECT * 
+    FROM Employees
+    WHERE manager_id IS NOT NULL
+        AND salary<30000
+)
+
+SELECT emp.employee_id
+FROM have_mng AS emp
+LEFT JOIN Employees AS mng
+    ON emp.manager_id = mng.employee_id
+WHERE mng.employee_id IS NULL
+ORDER BY emp.employee_id
+
+-- Simpler
+SELECT employee_id
+FROM Employees 
+WHERE salary <30000
+    AND manager_id NOT IN (SELECT employee_id FROM Employees)
+ORDER BY employee_id;
 
 -- EX 10
+SELECT COUNT(DISTINCT company_id) AS duplicate_companies
+FROM job_listings
+WHERE company_id IN
+  (
+    SELECT company_id
+    FROM job_listings
+    GROUP BY company_id, title, description
+    HAVING COUNT(title)>1
+  );
 
 -- EX 11
+-- Tạo bảng với user và số lần đánh giá; phim và trung bình rating trong T2/2020
+WITH user_rate_count AS 
+(
+    SELECT user_id
+    , COUNT(user_id) AS frequency
+    FROM MovieRating
+    GROUP BY user_id
+)
+, movie_rate20 AS 
+(
+    SELECT movie_id
+    , AVG(rating) AS avg_rating
+    FROM MovieRating 
+    WHERE EXTRACT (year FROM created_at) =2020
+        AND EXTRACT (month FROM created_at) =2
+    GROUP BY movie_id
+)
+-- Lấy name/title từ bảng tương ứng mà id bằng MAX số lần đánh giá/trung bình rating
+-- Order theo tên ASC -> limit 1 -> lấy 1 name/title theo lexicographically 
+-- Đóng ngoặc câu lệnh do dùng order by và limit để dùng được union all
+(
+SELECT name AS results
+FROM Users
+WHERE user_id IN 
+    (
+        SELECT user_id
+        FROM user_rate_count 
+        WHERE user_rate_count.frequency = (SELECT MAX(frequency) FROM user_rate_count)
+    )
+ORDER BY name ASC
+LIMIT 1 
+)
 
+UNION ALL
+
+(
+SELECT title
+FROM Movies
+WHERE movie_id IN 
+    (
+        SELECT movie_id
+        FROM movie_rate20
+        WHERE movie_rate20.avg_rating = (SELECT MAX(avg_rating) FROM movie_rate20)
+    )
+ORDER BY title ASC
+LIMIT 1 
+)
 
 -- EX 12
+WITH rest_friend AS
+(
+    SELECT requester_id AS id
+    , COUNT(requester_id) AS fr_count
+    FROM RequestAccepted
+    GROUP BY requester_id
+)
+, accept_friend AS
+(
+    SELECT accepter_id AS id
+    , COUNT(accepter_id) AS fr_count
+    FROM RequestAccepted
+    GROUP BY accepter_id
+)
+, all_friend AS
+(
+    SELECT *
+    FROM rest_friend
+    UNION ALL 
+    SELECT *
+    FROM accept_friend
+)
+, total_friend_sum AS
+(
+    SELECT id
+    , SUM(fr_count) AS num
+    FROM all_friend
+    GROUP BY id
+)
+
+SELECT *
+FROM total_friend_sum
+WHERE num = (SELECT MAX(num) FROM total_friend_sum)
+
+-- PROBLEM APP CLICKTHROUGH RATE IN VIDEO
+WITH ctr_table AS
+(
+  SELECT app_id
+  , SUM(
+    CASE
+      WHEN event_type = 'impression' THEN 1
+      ELSE 0
+    END)*1.00 AS count_impression
+  , SUM(
+    CASE
+      WHEN event_type = 'click' THEN 1
+      ELSE 0
+    END)*1.00 AS count_click
+  FROM events
+  WHERE EXTRACT(year FROM timestamp) = 2022
+  GROUP BY app_id
+)
+
+SELECT app_id
+, ROUND(100*(count_click/count_impression),2) AS ctr
+FROM ctr_table
+
+-- SOLUTION BY LESSON (using CTEs)
+
+-- CTEs lượng click, impression
+WITH no_clicks AS
+(
+  SELECT app_id
+  , COUNT(*) AS no_click
+  FROM events
+  WHERE EXTRACT(year FROM timestamp) = 2022
+    AND event_type = 'click'
+  GROUP BY app_id
+)
+, no_impressions AS
+(
+  SELECT app_id
+  , COUNT(*) AS no_impression
+  FROM events
+  WHERE EXTRACT(year FROM timestamp) = 2022
+    AND event_type = 'impression'
+  GROUP BY app_id
+)
+
+SELECT NI.app_id
+, ROUND(100.0*(NC.no_click/NI.no_impression),2) AS ctr
+FROM no_impressions AS NI
+LEFT JOIN no_clicks AS NC
+  ON NI.app_id = NC.app_id
+
+
